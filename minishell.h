e9350e8d5433751b32da79cd65f8b3838fe8cf41
Nicholas Ying Hao Loh <nloh@c1r6s3.42singapore.sg>
2025-11-25 15:39:24 +0800

    norm fix header file

--- minishell.h
+++ minishell.h
@@ -6,16 +6,16 @@
 /*   By: nloh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 15:08:49 by nloh              #+#    #+#             */
-/*   Updated: 2025/11/25 15:16:16 by nloh             ###   ########.fr       */
+/*   Updated: 2025/11/25 15:38:34 by nloh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 #endif
